f851d3b32eaedb7b8025b5277e78f380c190537d
x-heda <littleheda@outlook.com>
2025-07-09 22:19:18 -0400

    Update offworld.js

--- styles/js/pages/offworld.js
+++ styles/js/pages/offworld.js
@@ -8,6 +8,6 @@ import { charadex } from '../charadex.js';
 /* Load
 ======================================================================= */
 document.addEventListener("DOMContentLoaded", async () => {
-  let dex = await charadex.initialize.page(null, charadex.page.traits);
+  let dex = await charadex.initialize.page(null, charadex.page.offworld);
   charadex.tools.loadPage('.softload', 500);
 });
